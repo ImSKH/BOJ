cec9081384bcb45a5b5b1f5724fe2eda7f90e5ee
skihyeon1001 <82434047+skihyeon1001@users.noreply.github.com>
2021-04-16 22:50:16 +0900

    Update boj_1978.c

--- boj_1978.c
+++ boj_1978.c
@@ -1,123 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct _node{
-  int data;
-  struct _node *prev;
-  struct _node *next;
-}Node;
-
-typedef struct _deque{
-  Node *front;
-  Node *rear;
-  int size;
-} Deque;
-
-void MakeDeque(Deque *deque){
-  deque->front=deque->rear=NULL;
-  deque->size=0;
-}
-
-int IsEmpty(Deque *deque){
-  if(deque->size==0) return 1;
-  return 0;
-}
-
-void Push_front(Deque *deque, int data){
-  Node *newN=(Node *)malloc(sizeof(Node));
-  newN->data = data;
-  if(IsEmpty(deque)){
-    deque->front = deque -> rear = newN;
-    deque->front->prev = NULL;
-    deque->rear->next = NULL;
-    deque->size++;
-    return;
+int PrimeN(int N){
+  if(N==1) return 0;
+  else if(N==1) return 1;
+  else{
+    int cnt=0;
+    for(int i=2; i<N; i++){
+      if(N%i==0) cnt++;
+    }
+    if(cnt==0) return 1;
+    else return 0;
   }
-  newN->next=deque->front;
-  deque->front->prev=newN;newN->prev=NULL;
-  deque->front=newN;
-  deque->size++;
-}
-
-void Push_rear(Deque *deque, int data){
-  Node *newN=(Node *)malloc(sizeof(Node));
-  newN->data = data;
-  if(IsEmpty(deque)){
-    deque->front=deque->rear=newN;
-    deque->front->prev=NULL;
-    deque->rear->next=NULL;
-    deque->size++;
-    return;
-  }
-  newN->prev=deque->rear;
-  deque->rear->next=newN;
-  newN->next=NULL;
-  deque->rear=newN;
-  deque->size++;
-}
-
-int Pop_front(Deque *deque){
-  if(IsEmpty(deque)) return -1;
-  Node *curr = deque->front;
-  int re=0;
-  re = curr->data;
-  if(deque->size==1){
-    MakeDeque(deque);
-    return re;
-  }
-  curr->prev = NULL;
-  curr->next = deque->front->next;
-  deque->front = curr->next;
-  deque->size--;
-  return re;
-}
-
-int Pop_rear(Deque *deque){
-  if(IsEmpty(deque)) return -1;
-  Node *curr = deque->rear;
-  int re=0;
-  re = curr->data;
-  if(deque->size==1){
-    MakeDeque(deque);
-    return re;
-  }
-  curr->next = NULL;
-  curr->prev = deque->rear->prev;
-  deque->rear = curr->prev;
-  deque->size--;
-  return re;
-}
-
-int front(Deque *deque){
-  if(IsEmpty(deque)) return -1;
-  return deque->front->data;
-}
-
-int back(Deque *deque){
-  if(IsEmpty(deque)) return -1;
-  return deque->rear->data;
 }
 
 int main(){
-  int N,X;
-  char O[12];
-  Deque *deque=(Deque *)malloc(sizeof(Deque));
-  MakeDeque(deque);
-  scanf("%d",&N);
-  for(int i=0; i<N; i++){
-    scanf("%s",O);
-    if(O[0]=='s') printf("%d\n",deque->size);
-    else if(O[0]=='e') printf("%d\n",IsEmpty(deque));
-    else if(O[0]=='f') printf("%d\n",front(deque));
-    else if(O[0]=='b') printf("%d\n",back(deque));
-    else if(O[1]=='u'){
-      scanf("%d",&X);
-      if(O[5]=='f') Push_front(deque,X);
-      else Push_rear(deque, X);
-    }
-    else if(O[1]=='o'){
-      if(O[4]=='f') printf("%d\n",Pop_front(deque));
-      else printf("%d\n",Pop_rear(deque));
-    }
+  int T,N,cnt=0;
+  scanf("%d",&T);
+  for(int i=0; i<T; i++){
+    scanf("%d",&N);
+    if(PrimeN(N)==1) cnt++;
   }
+  printf("%d",cnt);
 }
